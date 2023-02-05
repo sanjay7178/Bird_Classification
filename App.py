3d1fdd2ad8aadd6346407c5e7453276fddfabfa2
sanjay7178 <saisanjay7660@gmail.com>
2023-02-05 21:33:37 +0530

    added .json few .html pages

--- App.py
+++ App.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template,Response, request
 import web_utils
 
+
+
 app=Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -17,18 +19,34 @@ def login():
         return render_template('index.html')
 
     return render_template('login.html')
-
+    
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         print('register post')
-        return render_template('login.html')
+        return render_template('register.html')
 
     return render_template('register.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')  
 
 @app.route('/video')
 def video():
     return render_template('video.html')  
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')  
+
+@app.route('/logs')
+def logs():
+    return render_template('logs.html')  
+
+@app.route('/error404')
+def error404():
+    return render_template('error-404.html')  
 
 @app.route('/video-stream')
 def video_stream():
